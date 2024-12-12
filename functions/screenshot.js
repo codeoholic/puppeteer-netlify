192e09df9e087bec86b18f711715885889dbb278
codeoholic <mohit@hey.com>
2024-12-12 11:46:58 +0530

    x

--- functions/screenshot.js
+++ functions/screenshot.js
@@ -2,34 +2,50 @@ const chromium = require("chrome-aws-lambda")
 
 exports.handler = async (event, context) => {
 
-  try {
+    try {
 
-    const browser = await chromium.puppeteer.launch({
+        const browser = await chromium.puppeteer.launch({
 
-        executablePath: await chromium.executablePath,
-        args: chromium.args,
-        defaultViewport: chromium.defaultViewport,
-        headless: chromium.headless,
-        ignoreHTTPSErrors: true,
+            executablePath: await chromium.executablePath,
+            args: chromium.args,
+            defaultViewport: chromium.defaultViewport,
+            headless: chromium.headless,
+            ignoreHTTPSErrors: true,
 
-    });
+        });
 
-    const page = await browser.newPage();
-    await page.goto('https://www.blupp.co');
-    const screenshot = await page.screenshot({ encoding: 'base64' });
+        const page = await browser.newPage();
+        await page.goto('https://www.blupp.co');
+        const screenshot = await page.screenshot({ encoding: 'base64' });
 
-    await browser.close();
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ screenshot: screenshot }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: error.toString() }),
-    };
-  }
-};
+        await browser.close();
+        return {
+
+            statusCode: 200,
+            body: JSON.stringify({ screenshot: screenshot }),
+            headers: {
+            'Content-Type': 'application/json',
+            },
+
+        };
+
+    } catch (error) {
+
+        console.error(error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: error.toString() }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
+
+    } finally {
+
+        if (browser) {
+            await browser.close();
+        }
+
+    }
+
+}
